01993a9fae3ad0349a921f57ffff044e27c82bd4
Marcelorose <marcelorose@edu.univali.br>
2017-12-15 12:06:24 -0200

    Com timeline

--- RedeSocial/LinkedLists.h
+++ RedeSocial/LinkedLists.h
@@ -2,9 +2,91 @@
 #define LINKEDLISTS_H
 #include <string>
 #include <iostream>
-
+#include <stdio.h>
+#include <time.h>
 
 using namespace std;
+
+
+struct Data {
+	int dia;
+	int mes;
+	int ano;
+};
+
+struct Post {
+	string criadorPost;
+	string oPost;
+	int numCaracteres;
+
+	Post() {
+		string umPost;
+		cin >> umPost;
+		this->oPost = umPost;
+	}
+
+	void toView() {
+		cout << "Criador do Post: " << this->criadorPost << " | Post: " << this->oPost;
+	}
+};
+
+struct Usuario {
+	string userName;
+	string senha;
+	string confirmacaoSenha;
+	string nomeCompleto;
+	Data dataDeNascimento;
+	string genero;
+	string post;
+
+	Usuario() {
+		//Usuario *novo = new Usuario;
+		string umUserName;
+		string umaSenha;
+		string umaConfirmacaoSenha;
+		string umNomeCompleto;
+		Data umaDataDeNascimento;
+		string umGenero;
+		cout << "Nome de Usuario: ";
+		cin >> umUserName;
+		cout << "Senha: ";
+		cin >> umaSenha;
+		cout << "Confirme sua senha: ";
+		cin >> umaConfirmacaoSenha;
+		while (umaConfirmacaoSenha != umaSenha)
+		{
+			cout << "\nAs senhas nao batem, digite a confirmacao de novo \nConfirmacao senha: ";
+			cin >> umaConfirmacaoSenha;
+			system("cls");
+		}
+		cout << "Nome Completo: ";
+		cin >> umNomeCompleto;
+		cout << "Data de nascimento: \nDia: ";
+		cin >> umaDataDeNascimento.dia;
+		cout << "Mes: ";
+		cin >> umaDataDeNascimento.mes;
+		cout << "Ano: ";
+		cin >> umaDataDeNascimento.ano;
+		cout << "Genero: ";
+		cin >> umGenero;
+		this->userName = umUserName;
+		this->senha = umaSenha;
+		this->confirmacaoSenha = umaConfirmacaoSenha;
+		this->nomeCompleto = umNomeCompleto;
+		this->dataDeNascimento.dia = umaDataDeNascimento.dia;
+		this->dataDeNascimento.mes = umaDataDeNascimento.mes;
+		this->dataDeNascimento.ano = umaDataDeNascimento.ano;
+		this->genero = umGenero;
+	}
+
+	void toView() {
+		cout << "Nome de Usuario: " << this->userName << " | Senha: " << this->senha << " | Senha confirmada: " << this->confirmacaoSenha <<
+			" | Nome completo: " << this->nomeCompleto << " | Data de nascimento: " << this->dataDeNascimento.dia << "/" << this->dataDeNascimento.mes <<
+			"/" << this->dataDeNascimento.ano << " | Genero: " << this->genero;
+
+	}
+
+};
 
 
 struct ElementoUsuario {
@@ -25,11 +107,15 @@ struct ListaUsuario {
 		this->primeiro = primeiro;
 	}
 
+	ListaUsuario() {
+		this->primeiro = NULL;
+	}
+
 	void mostrarElementos() {
 		ElementoUsuario *atual = this->primeiro;
 
 		while (atual != NULL) {
-			atual->usuario->toview();
+			atual->usuario->toView();
 			cout << endl;
 			atual = atual->proximoElemento;
 		}
@@ -53,12 +139,202 @@ struct ListaUsuario {
 		}
 	}
 
+	void verificarMaioridade(Usuario *Usuario) {
+		char dateStr[9];
+		_strdate_s(dateStr);
+
+		int dia;
+		int mes;
+		int ano;
+
+		if (dateStr[0] == '0')
+		{
+			mes = 0;
+		}
+		else if (dateStr[0] == '1')
+		{
+			mes = 10;
+		}
+		if (dateStr[1] == '0')
+		{
+			mes += 0;
+		}
+		else if (dateStr[1] == '1')
+		{
+			mes += 1;
+		}
+		else if (dateStr[1] == '2')
+		{
+			mes += 2;
+		}
+		if (dateStr[3] == '0')
+		{
+			dia = 0;
+		}
+		else if (dateStr[3] == '1')
+		{
+			dia = 10;
+		}
+		else if (dateStr[3] == '2')
+		{
+			dia = 20;
+		}
+		else if (dateStr[3] == '3')
+		{
+			dia = 30;
+		}
+		if (dateStr[4] == '0')
+		{
+			dia += 0;
+		}
+		else if (dateStr[4] == '1')
+		{
+			dia += 1;
+		}
+		else if (dateStr[4] == '2')
+		{
+			dia += 2;
+		}
+		else if (dateStr[4] == '3')
+		{
+			dia += 3;
+		}
+		else if (dateStr[4] == '4')
+		{
+			dia += 4;
+		}
+		else if (dateStr[4] == '5')
+		{
+			dia += 5;
+		}
+		else if (dateStr[4] == '6')
+		{
+			dia += 6;
+		}
+		else if (dateStr[4] == '7')
+		{
+			dia += 7;
+		}
+		else if (dateStr[4] == '8')
+		{
+			dia += 8;
+		}
+		else if (dateStr[4] == '9')
+		{
+			dia += 9;
+		}
+		if (dateStr[6] == '0')
+		{
+			ano = 0;
+		}
+		else if (dateStr[6] == '1')
+		{
+			ano = 10;
+		}
+		else if (dateStr[6] == '2')
+		{
+			ano = 20;
+		}
+		else if (dateStr[6] == '3')
+		{
+			ano = 30;
+		}
+		else if (dateStr[6] == '4')
+		{
+			ano = 40;
+		}
+		else if (dateStr[6] == '5')
+		{
+			ano = 50;
+		}
+		else if (dateStr[6] == '6')
+		{
+			ano = 60;
+		}
+		else if (dateStr[6] == '7')
+		{
+			ano = 70;
+		}
+		else if (dateStr[6] == '8')
+		{
+			ano = 80;
+		}
+		else if (dateStr[6] == '9')
+		{
+			ano = 90;
+		}
+		if (dateStr[7] == '0')
+		{
+			ano += 0;
+		}
+		else if (dateStr[7] == '1')
+		{
+			ano += 1;
+		}
+		else if (dateStr[7] == '2')
+		{
+			ano += 2;
+		}
+		else if (dateStr[7] == '3')
+		{
+			ano += 3;
+		}
+		else if (dateStr[7] == '4')
+		{
+			ano += 4;
+		}
+		else if (dateStr[7] == '5')
+		{
+			ano += 5;
+		}
+		else if (dateStr[7] == '6')
+		{
+			ano += 6;
+		}
+		else if (dateStr[7] == '7')
+		{
+			ano += 7;
+		}
+		else if (dateStr[7] == '8')
+		{
+			ano += 8;
+		}
+		else if (dateStr[7] == '9')
+		{
+			ano += 9;
+		}
+
+		ano += 2000;
+
+		if (Usuario->dataDeNascimento.ano >= 2000)
+		{
+			if (Usuario->dataDeNascimento.ano - ano < 18)
+			{
+				cout << "Menor de idade!";
+			}
+		}
+		
+	}
+
 	bool verificarUsuario(Usuario *novoUsuario) {
-		ElementoUsuario *atual = this->primeiro; \
+		ElementoUsuario *atual = this->primeiro;
+		string nomeUsuario;
+			verificarMaioridade(novoUsuario);
 			while (atual != NULL) {
 				if (novoUsuario->userName == atual->usuario->userName) {
+					do
+					{
+						cout << "\nUsername ja usado\n\nNome de Usuario: ";
+						cin >> nomeUsuario;
+						novoUsuario->userName = nomeUsuario;
+					} while (novoUsuario->userName == atual->usuario->userName);
+					
 					return true;
 					break;
+				}
+				else if (true)
+				{
+
 				}
 				atual = atual->proximoElemento;
 			}
@@ -74,10 +350,19 @@ struct ListaUsuario {
 				if (atual->usuario->senha == senha)
 				{
 					cout << "Logado\n";
+					system("cls");
 					return;
 				}
-				cout << "Errou a senha chefia\n";
-				return;
+				do
+				{
+					cout << "Senha errada! Tente novamente \nSenha: ";
+					cin >> senha;
+				} while (atual->usuario->senha != senha);	
+			}
+			else if (atual->usuario->userName != userName)
+			{
+				cout << "Este usuario nao existe! Tente novamente \nUsername: ";
+				cin >> userName;
 			}
 
 			atual = atual->proximoElemento;
@@ -105,6 +390,10 @@ struct ListaPost {
 		this->primeiro = primeiro;
 	}
 
+	ListaPost() {
+		this->primeiro = NULL;
+	}
+
 	void adicionarPost(ElementoPost *novoPost) {
 
 		if (this->primeiro == NULL) {
@@ -120,6 +409,22 @@ struct ListaPost {
 				atual = atual->proximoElemento;
 			}
 		}
+	}
+
+	void mostrarElementos() {
+		ElementoPost *atual = this->primeiro;
+
+		if (atual == NULL)
+		{
+			cout << "Nao existe nenhum post ainda\n";
+		}
+
+		while (atual != NULL) {
+			atual->post->toView();
+			cout << endl;
+			atual = atual->proximoElemento;
+		}
+
 	}
 
 };
